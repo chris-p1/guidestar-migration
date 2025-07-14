13a9914fc611b9d2a80326b2b0c9897e7be301fe
chris-p1 <chris.perez@wizehive.com>
2025-07-14 12:34:01 -0600

    wip: stage/prod creds

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
+/*
+What is this?
+
+This is a script to migrate the Firebase data from the old Guidestar plugin to
+the new one. The old one has a slightly older format that needs to be tweaked a
+bit to work with the new plugin.
+*/
+
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 
@@ -12,12 +21,17 @@ import (
 )
 
 func main() {
-	fmt.Println(reverse.String("Hello"))
+	fmt.Println(reverse.String("Guidestar Migration"))
 
-	opt := option.WithCredentialsFile("config/guidestar-multiconfig-firebase-adminsdk-pzpvm-0fcc9de3b9.json")
+	// 20250714T114955 TODO: change to prod once ready
+	testWs := "15032"
+	// fbUrl := "https://guidestar-multiconfig.firebaseio.com"
+	// opt := option.WithCredentialsFile("config/guidestar-multiconfig-firebase-adminsdk-pzpvm-0fcc9de3b9.json")
+	fbUrl := "https://guidestar-stage.firebaseio.com"
+	opt := option.WithCredentialsFile("config/guidestar-stage-firebase-adminsdk-7j26g-c38da55334.json")
 	ctx := context.Background()
 	conf := &firebase.Config{
-		DatabaseURL: "https://guidestar-multiconfig.firebaseio.com",
+		DatabaseURL: fbUrl,
 	}
 
 	app, err := firebase.NewApp(ctx, conf, opt)
@@ -30,7 +44,7 @@ func main() {
 		log.Fatalln("Error initializing database client:", err)
 	}
 
-	ref := client.NewRef("38099")
+	ref := client.NewRef(testWs)
 	var data map[string]interface{}
 
 	ref.Get(ctx, &data)
@@ -38,6 +52,7 @@ func main() {
 		log.Fatalln("Error reading from database:", err)
 	}
 
-	fmt.Println(data)
+	test, _ := json.Marshal(data)
+	fmt.Println(string(test))
 
 }
